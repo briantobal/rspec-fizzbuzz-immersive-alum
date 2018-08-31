817a98c2dda6677342d6f427cbcc922ac6ac92b6
Brian Tobal <brian.tobal@gmail.com>
2018-08-31 19:37:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,12 @@
 
 
 def fizzbuzz(num)
-  if num % 3 == 0
+  if num % 5 == 0 && num % 3 == 0 
+    return "FizzBuzz"
+  elsif num % 3 == 0
     return "Fizz"
   elsif num % 5 == 0 
     return "Buzz"
-  elsif num % 5 == 0 && num % 3 == 0 
-    return "FizzBuzz"
   else
     return nil 
   end
